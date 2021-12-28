33ea24abc93ba9bad747ede9a84b6f66ecba1cc5
JM-Duval <jmarieduval@hotmail.fr>
2021-12-28 11:58:59 +0100

    update setup config

--- lettings/tests.py
+++ lettings/tests.py
@@ -11,27 +11,25 @@ class TestLetting:
     def letting(self):
         self.fake = Faker()
         self.address = Address.objects.create(
-            number = self.fake.building_number(),
-            street = self.fake.street_name(),
-            city = self.fake.city(),
-            state = self.fake.name(),
-            zip_code = self.fake.postcode(),
+            number=self.fake.building_number(),
+            street=self.fake.street_name(),
+            city=self.fake.city(),
+            state=self.fake.name(),
+            zip_code=self.fake.postcode(),
         )
         self.letting = Letting.objects.create(
-            title = self.fake.name(), 
-            address = self.address
+            title=self.fake.name(),
+            address=self.address
         )
 
     def test_index(self, client):
         self.letting()
         response = client.get('/lettings/')
         assert response.context['lettings_list'][0] == self.letting
-        assertTemplateUsed(response, 'lettings_index.html')    
+        assertTemplateUsed(response, 'lettings_index.html')
 
     def test_letting(self, client):
         self.letting()
-        response = client.get(reverse('lettings:letting', kwargs={"letting_id":1}))
-        #print(response.content.decode())
+        response = client.get(reverse('lettings:letting', kwargs={"letting_id": 1}))
         assert response.context['title'] == self.letting.title
         assertTemplateUsed(response, 'letting.html')
-
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,5 +1,4 @@
 import pytest
-from django.test import Client
 from pytest_django.asserts import assertTemplateUsed
 from django.urls import reverse
 from .models import Profile
@@ -14,19 +13,19 @@ class TestProfile:
         self.fake = Faker()
         self.user = User.objects.create(username='Testuser')
         self.profile = Profile.objects.create(
-            user = self.user,
-            favorite_city = self.fake.city(),
+            user=self.user,
+            favorite_city=self.fake.city(),
         )
 
     def test_profiles_index(self, client):
         self.profile()
         response = client.get('/profiles/')
         assert response.context['profiles_list'][0] == self.profile
-        assertTemplateUsed(response, 'profiles_index.html')        
+        assertTemplateUsed(response, 'profiles_index.html')
 
     def test_profile(self, client):
         self.profile()
         response = client.get('/profiles/')
-        response = client.get(reverse('profiles:profile', kwargs={"username":'Testuser'}))
+        response = client.get(reverse('profiles:profile', kwargs={"username": 'Testuser'}))
         assert response.context['profile'] == self.profile
         assertTemplateUsed(response, 'profile.html')
