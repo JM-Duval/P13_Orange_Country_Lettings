40f0c7ab5d4aaecd1d10f511eab36f0e3020baca
JM-Duval <jmarieduval@hotmail.fr>
2022-01-13 08:36:58 +0100

    add sentry config

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -115,7 +115,8 @@ STATIC_URL = '/static/'
 
 
 sentry_sdk.init(
-    dsn="https://81aad97127354f1c9b8ca2905e114ede@o1112541.ingest.sentry.io/6142236",
+    dsn= os.getenv(KEY_SENTRY),
+    #dsn="https://81aad97127354f1c9b8ca2905e114ede@o1112541.ingest.sentry.io/6142236",
     integrations=[DjangoIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%
